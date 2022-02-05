234580ddcc1e5d356f25a761dbc7e2f5ec90da8a
SAGAR AMRUTH <sagaramruth1212@gmail.com>
2022-02-06 00:37:19 +0530

    feat

--- accordian.js
+++ accordian.js
@@ -1,5 +1,6 @@
 document.querySelectorAll('.accordian__button').forEach(button => {
     button.addEventListener('click', () => {
+        
         // const accordianContent = button.nextElementSibling;
 
         button.classList.toggle('accordian__button--active');
